0659cfe17851ba16b81bc55570b635544abbf3c2
Wasabi_Thumbs <wasabithumbs@gmail.com>
2017-07-10 11:06:44 -0400

    Fix file.Exists() syntax error

--- lua/autorun/server/wvt/ssp.lua
+++ lua/autorun/server/wvt/ssp.lua
@@ -3,7 +3,7 @@ local fName = ("ssp_data/" .. game.GetMap() .. "_ssp.txt")
 
 local function addSpawnPoint( vec )
 local cont = { vec }
-if file.Exists( fName ) then
+if file.Exists( fName, "DATA" ) then
 local ftable = util.JSONToTable( file.Read( fName ) )
 cont = table.Add(ftable, cont)
 else if !file.Exists( "ssp_data", "DATA" ) then
@@ -18,7 +18,7 @@ if file.Exists( fName ) then file.Delete( fName ) end
 end
 
 hook.Add( "PlayerSpawn", "ssp_redirector", function()
-if file.Exists( fName ) then
+if file.Exists( fName, "DATA" ) then
 local ftable = util.JSONToTable( file.Read( fName ) )
 ply:SetPos( table.Random( ftable ) )
 end
