750469f919f8680d631e2028e242cc37a8179fcb
Wasabi_Thumbs <wasabithumbs@gmail.com>
2017-07-10 11:24:56 -0400

    Started base for spawn customiser

--- lua/autorun/server/wvt/ssp.lua
+++ lua/autorun/server/wvt/ssp.lua
@@ -1,5 +1,6 @@
 AddCSLuaFile("lua/autorun/client/wvt/ssp.lua")
 local fName = ("ssp_data/" .. game.GetMap() .. "_ssp.txt")
+local cspName = ("ssp_data/" .. game.GetMap() .. "_ssp_type.txt")
 
 local function addSpawnPoint( vec )
 local cont = { vec }
@@ -14,7 +15,24 @@ file.Write( fName, cJSON )
 end
 
 local function clearSpawnPoints()
-if file.Exists( fName ) then file.Delete( fName ) end
+if file.Exists( fName, "DATA" ) then file.Delete( fName ) end
+end
+  
+local function customiseSpawn( stype )
+   if isValidType( stype ) then
+      if !( file.Exists( "ssp_data", "DATA" ) ) then file.CreateDir("ssp_data") end
+      file.Write( cspName, stype )
+   end
+end
+
+local function isValidType( stype )
+    if (stype == "ulaunch") then
+      return true
+    else if (stype == "flaunch") then
+      return true
+    else
+      return false
+     end
 end
 
 hook.Add( "PlayerSpawn", "ssp_redirector", function()
